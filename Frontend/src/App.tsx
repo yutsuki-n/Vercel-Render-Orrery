be3226ecd9625dedaccefe25a8add69914a78807
yutsuki <yutsuki.main.add@gmail.com>
2025-10-11 18:47:10 +0900

    complete Tailwind env

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,12 @@
-import './App.css'
+import { Button } from "@/components/ui/button";
 
 function App() {
-
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="min-h-screen bg-blue-500 flex items-center justify-center text-white text-3xl">
+      Tailwind 動作確認中
+      <Button>shadcn Button Test</Button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
